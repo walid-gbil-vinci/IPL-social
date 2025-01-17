62f335640d9190ca55f18c8502ae67182830b1f2
walid.gbil <walid.gbil@student.vinci.be>
2025-01-17 17:14:27 +0100

    correction du passwordCheker

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,14 +1,14 @@
 function passwordChecker(password) {
-    // Convertir en chaîne et nettoyer
+
     password = String(password).normalize('NFC').trim();
 
-    // Vérifications
-    if (password.length < 8) return false; // Vérifie la longueur
-    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) return false; // Vérifie les caractères spéciaux
-    if (!/\d/.test(password)) return false; // Vérifie les chiffres
-    if (/IPL/i.test(password)) return false; // Vérifie la présence de "IPL" (insensible à la casse)
 
-    return true; // Tout est valide
+    if (password.length < 8) return false; 
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) return false; 
+    if (!/\d/.test(password)) return false;
+    if (/IPL/i.test(password)) return false; 
+
+    return true; 
 }
 
 module.exports = passwordChecker;
